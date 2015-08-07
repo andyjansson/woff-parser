ed4ab7c20ab633c2796784ddf882b5d522d79f4b
andyjansson <andyjansson@users.noreply.github.com>
2015-08-07 09:00:03 +0200

    Change to unsigned integers

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ var os2 = require('./lib/os2');
 
 module.exports = function (data) {
 	return new Promise(function (resolve, reject) {
-		var signature = data.readInt32BE(0);
+		var signature = data.readUInt32BE(0);
 		if (signature !== 0x774F4646) return reject();
 
 		var woff = {
-			flavor: (data.readInt32BE(4) === 0x4F54544F ? 'CFF' : 'TrueType'),
-			totalSfntSize: data.readInt32BE(16),
-			majorVersion: data.readInt16BE(20),
-			minorVersion: data.readInt16BE(22)
+			flavor: (data.readUInt32BE(4) === 0x4F54544F ? 'CFF' : 'TrueType'),
+			totalSfntSize: data.readUInt32BE(16),
+			majorVersion: data.readUInt16BE(20),
+			minorVersion: data.readUInt16BE(22)
 		};
 		
-		var numTables = data.readInt16BE(12);
+		var numTables = data.readUInt16BE(12);
 		var dataTables = [];
 		for (var i = 0; i < numTables; i++) {
 			var tag = data.readInt32BE(44 + i * 20);
-			var offset = data.readInt32BE(48 + i * 20);
-			var compLength = data.readInt32BE(52 + i * 20);
-			var origLength = data.readInt32BE(56 + i * 20);
-			var origChecksum = data.readInt32BE(60 + i * 20);
+			var offset = data.readUInt32BE(48 + i * 20);
+			var compLength = data.readUInt32BE(52 + i * 20);
+			var origLength = data.readUInt32BE(56 + i * 20);
+			var origChecksum = data.readUInt32BE(60 + i * 20);
 			var buf = data.slice(offset, offset + compLength);
 			
 			switch (tag) {
